34a8e3b1d15e5b596815c3a1011e000587588677
MAHossain1 <mahpro110@gmail.com>
2023-07-07 08:08:11 +0600

    order summary on checkout page done

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -12,7 +12,6 @@ import {
   HiOutlineTrash,
 } from 'react-icons/hi';
 import { Button } from './ui/button';
-import { IProduct } from '@/types/globalTypes';
 import { useAppDispatch, useAppSelector } from '@/redux/hook';
 import {
   addToCart,
@@ -21,14 +20,8 @@ import {
 } from '@/redux/features/cart/cartSlice';
 
 export default function Cart() {
-  const { products } = useAppSelector((state) => state.cart);
+  const { products, total } = useAppSelector((state) => state.cart);
   const dispatch = useAppDispatch();
-
-  //! Dummy data
-
-  const total = 0;
-
-  //! **
 
   return (
     <Sheet>
