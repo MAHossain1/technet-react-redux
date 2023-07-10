8a55c1d0355b7059d02f5f4e5afcbdc743cede52
MAHossain1 <mahpro110@gmail.com>
2023-07-10 16:39:52 +0600

    login and signup done using createAsyncThunk done

--- src/redux/features/user/userSlice.ts
+++ src/redux/features/user/userSlice.ts
@@ -1,6 +1,9 @@
 import { auth } from '@/lib/firebase';
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from 'firebase/auth';
 
 interface IUser {
   user: {
@@ -34,6 +37,15 @@ export const createUser = createAsyncThunk(
   }
 );
 
+export const loginUser = createAsyncThunk(
+  'user/loginUser',
+  async ({ email, password }: ICredential) => {
+    const data = await signInWithEmailAndPassword(auth, email, password);
+
+    return data.user.email;
+  }
+);
+
 const userSlice = createSlice({
   name: 'user',
   initialState,
@@ -51,6 +63,22 @@ const userSlice = createSlice({
       })
       .addCase(createUser.rejected, (state, action) => {
         state.user.email = null;
+        state.isLoading = false;
+        state.isError = true;
+        state.error = action.error.message!;
+      })
+      .addCase(loginUser.pending, (state) => {
+        state.isLoading = true;
+        state.isError = false;
+        state.error = null;
+      })
+      .addCase(loginUser.fulfilled, (state, action) => {
+        state.user.email = action.payload;
+        state.isLoading = false;
+      })
+      .addCase(loginUser.rejected, (state, action) => {
+        state.user.email = null;
+        state.isLoading = false;
         state.isError = true;
         state.error = action.error.message!;
       });
